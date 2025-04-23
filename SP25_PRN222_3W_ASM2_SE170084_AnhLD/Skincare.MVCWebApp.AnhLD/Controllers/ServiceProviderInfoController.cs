62866bda6c0f832af24b3053ef896e5d009b2de5
duc-anh-le <ducanhle.iter@gmail.com>
2025-04-23 08:43:42 +0700

    update

---
+++ SP25_PRN222_3W_ASM2_SE170084_AnhLD/Skincare.MVCWebApp.AnhLD/Controllers/ServiceProviderInfoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Skincare.Services.Interface;
+
+namespace Skincare.MVCWebApp.AnhLD.Controllers
+{
+    public class ServiceProviderInfoController : Controller
+    {
+        private readonly IServiceProviders _serviceProviders;
+
+        public ServiceProviderInfoController(IServiceProviders serviceProviders)
+        {
+            _serviceProviders = serviceProviders;
+        }
+
+        public IActionResult Index()
+        {
+            var serviceProviders = _serviceProviders.ServiceProviderInfoService.GetAllServiceProvidersAsync();
+            return View(serviceProviders);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var serviceProvider = await _serviceProviders.ServiceProviderInfoService.GetServiceProviderByIdAsync(id);
+            if (serviceProvider == null)
+            {
+                return NotFound();
+            }
+            return View(serviceProvider);
+        }
+    }
+}
