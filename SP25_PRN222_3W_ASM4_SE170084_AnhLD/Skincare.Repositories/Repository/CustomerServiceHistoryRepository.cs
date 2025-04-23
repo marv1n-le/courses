cc6b24e40d166c1dab4a807d0b7002a6858bf716
duc-anh-le <ducanhle.iter@gmail.com>
2025-04-24 01:32:26 +0700

    create asm4

---
+++ SP25_PRN222_3W_ASM4_SE170084_AnhLD/Skincare.Repositories/Repository/CustomerServiceHistoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Skincare.Repositories.Base;
+using Skincare.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skincare.Repositories.Repository
+{
+    public class CustomerServiceHistoryRepository : GenericRepository<CustomerServiceHistory>
+    {
+        public CustomerServiceHistoryRepository() : base()
+        {
+        }
+       
+        public CustomerServiceHistoryRepository(SP25_PRN222_W3_PRJ_G1_SkinCareBookingServiceContext context) => _context = context;
+    }
+}
